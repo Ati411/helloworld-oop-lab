7cd60d85412a3d5b4e5bf07b82173d9373cc5d9b
Ati411 <athiwat.t@kkumail.com>
2024-11-25 20:15:26 -0800

    add newproject

---
+++ Ati/Ati/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Athiwat Tangrusicharoen";
+string student = "673450041-1";
+// string is Datatype => ชนิดของข้อมูล
+// name is variable
+// ". . ." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My Student ID :" + student);
